544db2afc178ad1527c60282f78f58babd61db32
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-02-03 07:51:14 +0100

    Added UTest-Project

---
+++ AtlasWrapperUTest/AtlasWrapperUTest.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class AtlasWrapperUTest
+
+    Private ReadOnly GPSHost As String = "https://sample.serviceprovider.com/atlas"
+    Private ReadOnly GPSUser As String = "sampleUsername"
+    Private ReadOnly GPSPass As String = "samplePassword"
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
---
+++ AtlasWrapperUTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("AtlasWrapperUTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("AtlasWrapperUTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("d53c3175-3cbf-4e15-a9bf-e94667767087")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
