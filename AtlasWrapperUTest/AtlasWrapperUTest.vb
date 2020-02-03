15c52d5f4b4e89986c82353c88d474f1dae8c839
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-02-03 08:53:59 +0100

    Added Unit-Tests for Ignitions-Method

--- AtlasWrapperUTest/AtlasWrapperUTest.vb
+++ AtlasWrapperUTest/AtlasWrapperUTest.vb
@@ -8,7 +8,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     Private ReadOnly GPSHost As String = "https://sample.serviceprovider.com/atlas"
     Private ReadOnly GPSUser As String = "sampleUsername"
     Private ReadOnly GPSPass As String = "samplePassword"
-
+#Region "[TEST-METHOD : Devices]"
     <TestMethod()> Public Sub Devices_OVRLD1_DefaultUseCase_NoParams_ExpectResult()
         Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
         Dim result = fetcher.Devices()
@@ -30,4 +30,49 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Assert.AreNotEqual(result, -1)
         If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
     End Sub
+#End Region
+#Region "[TEST-METHOD : Ignitions]"
+    <TestMethod()> Public Sub Ignitions_OVRLD1_DefaultUseCase_DateRange0H_ExpectNull()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(Date.Now, Date.Now)
+        Assert.IsNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Ignitions_OVRLD1_DefaultUseCase_DateRange3H_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(Date.Now.AddHours(-3), Date.Now)
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Ignitions_OVRLD1_DefaultUseCase_DateRange24H_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(Date.Now.AddHours(-24), Date.Now)
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Ignitions_OVRLD2_DefaultUseCase_AllPast_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(Date.Now.AddHours(-5))
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Ignitions_OVRLD2_DefaultUseCase_AllFuture_ExpectNull()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(Date.Now.AddHours(5))
+        Assert.IsNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Ignitions_OVRLD2_DefaultUseCase_PastAndFuture_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Ignitions(DateTime.Now.AddHours(-1))
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+#End Region
 End Class
