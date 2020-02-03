aa000420665cc345bead5c0b0fb0b5da3e825333
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-02-03 08:38:08 +0100

    Added Unit-Test for Devices-Method

--- AtlasWrapperTester/AtlasWrapperSample.vb
+++ AtlasWrapperTester/AtlasWrapperSample.vb
@@ -1,7 +1,12 @@
 Imports System
+Imports AtlasWrapper
 
 Module AtlasWrapperSample
     Sub Main()
+
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper("", "", "")
+
+        Dim result = fetcher.Devices()
 
     End Sub
 End Module
--- AtlasWrapperUTest/AtlasWrapperUTest.vb
+++ AtlasWrapperUTest/AtlasWrapperUTest.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Text
+﻿Imports System
+Imports System.Text
+Imports AtlasWrapper
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class AtlasWrapperUTest
@@ -7,7 +9,25 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     Private ReadOnly GPSUser As String = "sampleUsername"
     Private ReadOnly GPSPass As String = "samplePassword"
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub Devices_OVRLD1_DefaultUseCase_NoParams_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Devices()
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
     End Sub
-
+    <TestMethod()> Public Sub Devices_OVRLD1_DefaultUseCase_Params1_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Devices(0)
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
+    <TestMethod()> Public Sub Devices_OVRLD1_DefaultUseCase_Params2_ExpectResult()
+        Dim fetcher As AtlasWrapper.AtlasWrapper = New AtlasWrapper.AtlasWrapper(GPSHost, GPSUser, GPSPass)
+        Dim result = fetcher.Devices(1)
+        Assert.IsNotNull(result)
+        Assert.AreNotEqual(result, -1)
+        If Not result Is Nothing Then Assert.IsFalse(result.ToString().ToLower().StartsWith("e-"))
+    End Sub
 End Class
